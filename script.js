575e2d5530948c0b6742ecd36a3d9e470b9e9d0f
Muhammad Munim Siddiqui <munim1siddiqui@gmail.com>
2024-07-06 07:31:43 -0700

    add programes

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1 :TABLE OF THE GIVEN NUMBER!
+
+// let num = +prompt("Give a number!");
+// for(let i = 0; i <= 10; i++) {
+//     console.log(`${num} * ${i} = ${num * i}`)
+// }
+
+// 2 : ADDING AND SUM OF THE ARRAY'S NUMBER !
+
+// let myArray = [];
+// let sum = 0;
+// for(let n=0; n<=4; n++) {
+//     let give = +prompt("Give a number!");
+//     myArray.push(give);
+// }
+// for(let s=0; s<=4; s++) {
+//     sum += myArray[s];
+// }
+// console.log(myArray);
+// console.log(`The sum of the array is ${sum}`)
